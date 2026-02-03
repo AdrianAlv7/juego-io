8a57c378f981f84b712b993b690564e4cce5f40c
AdrianAlv7 <watersandia14@gmail.com>
2026-02-03 17:48:06 -0600

    SUBIENDO ALL archivos
    TODO

---
+++ limite.tsx
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="limite" tilewidth="32" tileheight="32" tilecount="108" columns="18">
+ <image source="conjunto de patrones.png" width="583" height="211"/>
+ <tile id="5">
+  <objectgroup>
+   <object id="1" type="limite" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup>
+   <object id="1" type="limite" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup>
+   <object id="1" type="limite" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup>
+   <object id="1" type="limite" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <objectgroup>
+   <object id="1" type="limite" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="44">
+  <objectgroup>
+   <object id="1" type="limite" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="49">
+  <objectgroup>
+   <object id="1" type="limite" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="54">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="limite" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="55">
+  <objectgroup>
+   <object id="1" type="limite" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="62">
+  <objectgroup>
+   <object id="1" type="limite" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="67">
+  <objectgroup>
+   <object id="1" type="limite" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="80">
+  <objectgroup>
+   <object id="1" type="limite" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="98">
+  <objectgroup>
+   <object id="1" type="limite" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
